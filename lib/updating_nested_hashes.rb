478ee624c5fa7dd68819ff6318bf3d61b80a0ec9
Yu Shi <yushiys95@gmail.com>
2021-01-20 03:00:33 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -35,7 +35,7 @@ epic_tragedy = {
   :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine => {name: "Juliet", age: "15", status: "dead"},
       :heroine_friends => [
       {name: "Steven", age: "30", attitude: "confused"},
       {name: "Nurse", age: "44", attitude: "worried"}
